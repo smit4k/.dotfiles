f1c1ce7804a135e950bbd2ecd36ee2b41c06f2ac
Smit Patil <smit@smit.codes>
2025-11-30 01:21:36 -0500

    Update cord.lua

--- nvim/lua/plugins/cord.lua
+++ nvim/lua/plugins/cord.lua
@@ -1,7 +1,15 @@
 return {
   "vyfor/cord.nvim",
-  build = ":Cord update",
-  text = {
-    workspace = "Workspace: {workspace}",
+  opts = {
+    enabled = true,
+    build = ":Cord update",
+    editor = {
+      tooltip = "Neovim",
+    },
+    text = {
+      workspace = function(opts)
+        return "Workspace: " .. opts.workspace
+      end,
+    },
   },
 }
